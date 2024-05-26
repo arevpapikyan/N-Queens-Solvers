065eb06f415d55ceacfbcf0ab6a2034bc70520ea
arevpapikyan <arevpapikyan@gmail.com>
2024-05-26 04:33:53 +0400

    added the files

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# N-Queens-Solvers
-The N-Queens problem is a well-known combinatorial problem in which the task is to place n chess queens on an n x n chessboard such that no two queens are on the same row, column, or diagonal. 
+# Hill Climbing and Local Beam Search Algorithms in Python
+
+## Hill Climbing Search Algorithm
+
+This implementation includes three variants of the hill climbing search algorithm: basic hill climbing, hill climbing with sideways moves, and hill climbing with sideways moves and random restarts.
+
+### Functions
+
+#### 1. `hillClimbing`
+
+- **Parameters**: `coordinates` - the coordinates of the initial state board.
+- **Output**: Outputs either the solution or asserts that no solution was found.
+
+#### 2. `hillClimbingWithSidewaysMove`
+
+- **Parameters**:
+  - `coordinates` - the coordinates of the initial state board.
+  - `limit` - the limit of sideways moves.
+- **Output**: Outputs either the solution or asserts that no solution was found.
+
+#### 3. `hillClimbingWithSidewaysMoveRandomRestarts`
+
+- **Parameters**:
+  - `coordinates` - the coordinates of the initial state board.
+  - `limit` - the limit of sideways moves.
+  - `restarts` - the number of restarts allowed.
+- **Output**: Outputs either the solution or asserts that no solution was found.
+
+## Local Beam Search Algorithm
+
+This implementation includes a local beam search algorithm.
+
+### Function
+
+#### `localBeam`
+
+* **Parameters** :
+* `num_queens` - the number of queens.
+* `num_initial_states` - the number of initial states.
+* **Output** : Outputs either the solution or asserts that no solution was found.
---
+++ hillclimbing.py
@@ -0,0 +1,98 @@
+import pandas as pd
+import random
+import sys
+import copy
+import time
+
+# Set recursion limit to avoid stack overflow
+sys.setrecursionlimit(5000)
+
+def coordinatesToBoard(coordinates):
+    """
+    Convert coordinates to a board representation.
+    """
+    board = [["_"] * n for _ in range(n)]
+    for i in coordinates:
+        board[i[0]][i[1]] = 'Q'
+    return board
+
+def countAttacks(coordinates):
+    """
+    Count the number of attacks on a given board.
+    """
+    count = 0
+    for i in range(len(coordinates)):
+        for j in range(len(coordinates)):
+            if abs(coordinates[i][0] - coordinates[j][0]) == abs(coordinates[i][1] - coordinates[j][1]) and coordinates[i] != coordinates[j]:
+                count += 1  # Diagonal attack
+            if coordinates[i][0] == coordinates[j][0] and coordinates[i] != coordinates[j]:
+                count += 1  # Horizontal attack
+    return count
+
+def generateSuccessors(coordinates):
+    """
+    Generate successors for a given board state.
+    """
+    successors = []
+    for i in range(len(coordinates)):
+        current_column = coordinates[i][1]
+        for j in range(n):
+            if j != coordinates[i][0]:  # Avoid placing a queen in the same row
+                new_state = copy.deepcopy(coordinates)
+                new_state[i] = [j, current_column]
+                successors.append(new_state)
+    return successors
+
+def k_smallest(lst, attacks, k):
+    """
+    Return the k smallest elements from a list.
+    """
+    sorted_attacks = sorted(attacks)
+    smallest = []
+    for i in range(k):
+        ind = attacks.index(sorted_attacks[i])
+        if lst[ind] not in smallest:
+            smallest.append(lst[ind])
+    return smallest
+
+def localBeam(k, n):
+    """
+    Local Beam Search algorithm for the N-Queens problem.
+    """
+    def generate_initial_state(n):
+        return [[random.randint(0, n-1), i] for i in range(n)]
+    
+    def is_goal_state(coordinates):
+        return countAttacks(coordinates) == 0
+    
+    def local_beam_search(initial_states, k):
+        current_states = initial_states
+        limit = 100
+        while limit > 0:
+            limit -= 1
+            next_states = []
+            for state in current_states:
+                successors = generateSuccessors(state)
+                for successor in successors:
+                    if is_goal_state(successor):
+                        return successor
+                    next_states.append(successor)
+            attacks = [countAttacks(state) for state in next_states]
+            current_states = k_smallest(next_states, attacks, k)
+        if limit == 0 and 0 not in attacks:
+            return 'no solution was found in 100 steps'
+    
+    initial_states = [generate_initial_state(n) for _ in range(k)]
+    solution = local_beam_search(initial_states, k)
+    if isinstance(solution, list):
+        return pd.DataFrame(coordinatesToBoard(solution)).to_string(index=False, header=False)
+    else:
+        return solution
+
+n = 5
+k = 5
+start = time.time()
+solution = localBeam(k, n)
+end = time.time()
+print(solution)  # Print solution board
+print(end - start)  # Print time taken
